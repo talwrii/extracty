bc855790587bcdff9eff9dd54096cccccc9306f5
Tal Wrii <talwrii@gmail.com>
2016-10-13 16:01:04 -0500

    Extend options, use argparse
    The specific use case I was after is only showing the title.
    
    If this is implemented it becomes natural to only want to show any field
    
    This is achieved through a --include argument which defaults to all fields
    
    For completeness a --exclude field is added, and the previous --no-x
    fields are retained for backwards compatibility and work by
    modifying exclude.
    
    --exclude and --include work for all metadata that might be returned.
    There was an unfortunate collision between the url argument
    and the logical name the option to hide/show the url
    metadata, so a little argument renaming was necessary.
    
    argparse was used to make all this logic easy to understand.

--- extracty/__init__.py
+++ extracty/__init__.py
@@ -8,12 +8,15 @@
 
 """
 
+import argparse
 import re
 import urlparse
 import justext
 import lxml.html
 
+
 from .author import extract_author
+from .content import extract_content
 from .image import extract_cover_image
 from .title import extract_title
 from .content import extract_content
@@ -23,12 +26,15 @@ __all__ = (
     'extract', 'extract_author', 'extract_cover_image', 'extract_title',
     'html_to_text')
 
-def extract(doc, url, author=True, cover_image=True, title=True, content=True):
+def extract(doc, src, author=True, cover_image=True, title=True, content=True, url=True):
     """ Extract metadata from HTML document"""
     if isinstance(doc, basestring):
         doc = lxml.html.fromstring(doc)
 
-    metadata = {'url': url}
+    metadata = dict()
+
+    if url:
+        metadata['url'] = src
 
     if author:
         metadata['author'] = extract_author(doc)
@@ -37,45 +43,72 @@ def extract(doc, url, author=True, cover_image=True, title=True, content=True):
         metadata['title'] = extract_title(doc)
 
     if cover_image:
-        extracted = extract_cover_image(doc, url)
+        extracted = extract_cover_image(doc, src)
         if extracted:
-            extracted = urlparse.urljoin(url, extracted)
+            extracted = urlparse.urljoin(src, extracted)
         metadata['cover_image'] = extracted
 
     # this should go last, because it mutates tree
     if content:
-        metadata['content'] = extract_content(doc, url)
+        metadata['content'] = extract_content(doc, src)
 
     return metadata
 
+FIELDS = TITLE, AUTHOR, COVER_IMAGE, URL, CONTENT = 'title', 'author', 'cover_image', 'url', 'content'
+
 def main():
-    """usage: extracty [options] SRC
+    PARSER = argparse.ArgumentParser(description='Extract meta data from HTML pages')
+    PARSER.add_argument('-u', '--url', type=str, help='url to use in case of filename provided')
 
-    argument:
-        SRC     url or filename
+    PARSER.add_argument('src', type=str, help='url or filename')
 
-    options:
-        -h, --help          show this message and exit
-        -u, --url URL       url to use in case of filename provided
-        --no-cover-image    do not extract image
-        --no-author         do not extract author
-        --no-title          do not extract title
-    """
-    import docopt
+    PARSER.add_argument(
+        '--no-title', action='append_const', const=TITLE,
+        dest='exclude', help='do not extract title')
+    PARSER.add_argument(
+        '--no-author', action='append_const', const=AUTHOR,
+        dest='exclude', help='do not extract title')
+    PARSER.add_argument(
+        '--no-cover-image', action='append_const', const=COVER_IMAGE,
+        dest='exclude', help='do not extract title')
+    PARSER.add_argument(
+        '--exclude', '-x', choices=FIELDS,
+        dest='exclude', action='append', help='Do not show field')
+    PARSER.add_argument(
+        '--include', '-i',
+        choices=FIELDS, dest='include', action='append', help='Show this field')
+    args = PARSER.parse_args()
+
     import urllib2
-    args = docopt.docopt(main.__doc__)
+    args = PARSER.parse_args()
 
-    if args['SRC'].lower().startswith('http'):
-        url = args['SRC']
-        data = fetch_url(url)
+    if args.src.lower().startswith('http'):
+        src_url = args.src
+        data = fetch_url(src_url)
     else:
-        url = args['--url'] or ''
-        data = open(args['SRC']).read()
-    metadata = extract(data, url=url,
-        author=not args['--no-author'],
-        title=not args['--no-title'],
-        cover_image=not args['--no-cover-image'],
-        )
+        src_url = args.url or ''
+        with open(args.src) as stream:
+            data = stream.read()
+
+
+    included_fields = args.include or FIELDS
+    excluded_fields = args.exclude or []
+
+    author = AUTHOR in included_fields and AUTHOR not in excluded_fields
+    title = TITLE in included_fields and TITLE not in excluded_fields
+    cover_image = COVER_IMAGE in included_fields and COVER_IMAGE not in excluded_fields
+    content = CONTENT in included_fields and CONTENT not in excluded_fields
+    url = URL in included_fields and URL not in excluded_fields
+
+    metadata = extract(
+        data, src=src_url,
+        author=author, title=title, cover_image=cover_image,
+        content=content, url=url)
+
+    illegal_fields = set(metadata) - set(FIELDS)
+
+    assert not illegal_fields, illegal_fields
+
     for k, v in metadata.items():
         v = v or ''
         print '%s\t%s' % (k, v.encode('utf8'))
